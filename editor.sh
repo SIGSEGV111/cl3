5c3902491f231aa1362fe6769b66d7f1da1de602
Simon Brennecke <>
2014-09-06 14:26:53 +0200

    build-system: tweaks

--- editor.sh
+++ editor.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-edit-source src/
+exec >/dev/null 2>&1 </dev/null
+
+kate $(find src -iname "*.cpp" -or -iname "*.hpp" | grep -Fv "/test/") &
+kate $(find src -iname "*.cpp" -or -iname "*.hpp" | grep -F  "/test/") &
