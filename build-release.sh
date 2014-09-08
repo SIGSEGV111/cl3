462aac47a6252927a85de5945c5a420e98f2a57a
Simon Brennecke <simon.brennecke@sap.com>
2014-09-08 17:26:41 +0200

    build-system: fixed the make -j1 problem

--- build-release.sh
+++ build-release.sh
@@ -2,4 +2,4 @@
 
 source util/build-base.sh
 
-cd tmp/rel && cmake ../.. -DBUILD_TYPE=release && make -j $PJOBS || make -j 1
+cd tmp/rel && cmake ../.. -DBUILD_TYPE=release && (make -j $PJOBS || make -j 1)
