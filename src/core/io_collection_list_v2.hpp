1b41710bba43a541db794d887fda6dd0d7f9231a
Simon Brennecke <>
2018-10-12 12:17:14 +0200

    more code for TList2
    Change-Id: I0b1f789e1bb6c837f369f5d4003a5c56b71918c8

--- src/core/io_collection_list_v2.hpp
+++ src/core/io_collection_list_v2.hpp
@@ -13,19 +13,15 @@ namespace cl3
 			namespace list_v2
 			{
 				template<typename T>
-				struct is_expensive_to_copy
-				{
-					static const bool value = false;
-				};
-
-				template<typename T>
 				void MoveItems(T* const arr_source, T* const arr_dest, const usys_t n_move);
+
+				// NOTE: only accepts datatypes which are trivially moveable!
 
 				template<typename T>
 				class TList2
 				{
 					public:
-						using valuearg_t = typename std::conditional<(sizeof(T) <= sizeof(void*) && std::is_copy_constructible<T>::value && !is_expensive_to_copy<T>::value), const T, const T&>::type;
+						using valuearg_t = typename std::conditional<(sizeof(T) <= sizeof(void*) && std::is_trivially_copyable<T>::value), const T, const T&>::type;
 
 					protected:
 						T* arr_items;
@@ -35,6 +31,8 @@ namespace cl3
 						u8_t div_prealloc;
 						bool preserve_order;
 
+						inline usys_t NormalizeIndex(const ssys_t index) const CL3_GETTER;
+
 					public:
 						void Prealloc(const usys_t n_want);
 						void MoveAppend(T&& item_append);
@@ -42,59 +40,26 @@ namespace cl3
 						void Append(const usys_t n_items_append, const T* const arr_items_append);
 						void Clear();
 
-						CLASS TList2(const usys_t n_prealloc = 0);
+						inline usys_t Count() const CL3_GETTER { return this->n_items; }
+						inline T& operator[](const ssys_t index) CL3_GETTER;
+						inline valuearg_t operator[](const ssys_t index) const CL3_GETTER;
+
+						TList2& operator=(const TList2&);
+						TList2& operator=(TList2&&);
+
+						CLASS TList2(const usys_t n_prealloc = 0, const u16_t n_prealloc_static = 128U / sizeof(T), const u8_t div_prealloc = 64U, const bool preserve_order = true);
 						CLASS TList2(const TList2&);
 						CLASS TList2(TList2&&);
 						CLASS ~TList2();
 				};
 
-				template<bool>
-				void MoveItem(T* const item_source, T* const item_dest);
-
-				template<bool>
-				void MoveItem<true>(T* const item_source, T* const item_dest)
-				{
-				}
-
-				template<bool>
-				void MoveItem<false>(T* const item_source, T* const item_dest)
-				{
-				}
-
 				template<typename T>
-				void MoveItems(T* const arr_source, T* const arr_dest, const usys_t n_move)
+				usys_t TList2<T>::NormalizeIndex(const ssys_t index) const
 				{
-					if(std::is_trivially_move_constructible<T>::value)
-					{
-						memmove((void*)arr_dest, (void*)arr_source, n_move * sizeof(T));
-					}
+					if(CL3_LIKELY(index >= 0))
+						return index;
 					else
-					{
-						for(usys_t i = 0; i < n_move; i++)
-						{
-							try
-							{
-								if constexpr(std::is_move_constructible<T>::value)
-									new (arr_dest + i) T(system::def::move(arr_source[i]));
-								else
-									new (arr_dest + i) T(arr_source[i]);
-							}
-							catch(...)
-							{
-								// TODO: add annotation to error
-
-								// rollback
-								for(usys_t j = i-1; j != (usys_t)-1; j--)
-								{
-									if constexpr(std::is_move_constructible<T>::value)
-										arr_source[j] = system::def::move(arr_dest[j]);
-									arr_dest[j].~T();
-								}
-
-								throw;
-							}
-						}
-					}
+						return this->n_items - index;
 				}
 
 				template<typename T>
@@ -104,22 +69,8 @@ namespace cl3
 					{
 						const usys_t n_prealloc = CL3_MAX((this->n_prealloc_static + this->n_items / this->div_prealloc), n_want);
 
-						T* arr_items_new;
-
-						if(std::is_trivially_move_constructible<T>::value)
-							arr_items_new = (T*)realloc((void*)this->arr_items, sizeof(T) * (n_prealloc + this->n_items));
-						else
-							arr_items_new = (T*)malloc(sizeof(T) * (n_prealloc + this->n_items));
-
+						T* const arr_items_new = (T*)realloc((void*)this->arr_items, sizeof(T) * (n_prealloc + this->n_items));
 						CL3_CLASS_ERROR(arr_items_new == nullptr, error::TException, "out of memory during TList2 realloc (n_items: %zu, n_prealloc_current: %zu, n_want: %zu, n_prealloc: %zu, sizeof(T): %zu bytes)", this->n_items, this->n_prealloc_current, n_want, n_prealloc, sizeof(T));
-
-						if(!std::is_trivially_move_constructible<T>::value)
-						{
-							try { MoveItems(this->arr_items, arr_items_new, this->n_items); }
-							catch(...) { free(arr_items_new); throw; }
-
-							free(this->arr_items);
-						}
 
 						this->arr_items = arr_items_new;
 						this->n_prealloc_current = n_prealloc;
@@ -179,7 +130,63 @@ namespace cl3
 				}
 
 				template<typename T>
-				CLASS TList2<T>::TList2(const usys_t n_prealloc) : arr_items(nullptr), n_items(0), n_prealloc_current(0), n_prealloc_static(128U / sizeof(T)), div_prealloc(64), preserve_order(true)
+				T& TList2<T>::operator[](const ssys_t index)
+				{
+					const usys_t _index = this->NormalizeIndex(index);
+					return this->arr_items[_index];
+				}
+
+				template<typename T>
+				typename TList2<T>::valuearg_t TList2<T>::operator[](const ssys_t index) const
+				{
+					const usys_t _index = this->NormalizeIndex(index);
+					return this->arr_items[_index];
+				}
+
+				template<typename T>
+				TList2<T>& TList2<T>::operator=(const TList2& rhs)
+				{
+					this->Clear();
+					this->Prealloc(rhs.Count());
+
+					usys_t i = 0;
+					try
+					{
+						for(; i < rhs.Count(); i++)
+							new (this->arr_items + i) T(rhs.arr_items[i]);
+					}
+					catch(...)
+					{
+						for(i--; i != (usys_t)-1; i--)
+							this->arr_items[i].~T();
+
+						free(this->arr_items);
+						this->arr_items = nullptr;
+						throw;
+					}
+
+					this->n_items = rhs.n_items;
+					return *this;
+				}
+
+				template<typename T>
+				TList2<T>& TList2<T>::operator=(TList2&& rhs)
+				{
+					this->Clear();
+					this->arr_items = rhs.arr_items;
+					this->n_items = rhs.n_items;
+					this->n_prealloc_current = rhs.n_prealloc_current;
+					this->n_prealloc_static = rhs.n_prealloc_static;
+					this->div_prealloc = rhs.div_prealloc;
+					this->preserve_order = rhs.preserve_order;
+					rhs.arr_items = nullptr;
+					rhs.n_items = 0;
+					rhs.n_prealloc_current = 0;
+					return *this;
+				}
+
+				template<typename T>
+				CLASS TList2<T>::TList2(const usys_t n_prealloc, const u16_t n_prealloc_static, const u8_t div_prealloc, const bool preserve_order) : arr_items(nullptr), n_items(0), n_prealloc_current(0), n_prealloc_static(n_prealloc_static), div_prealloc(div_prealloc), preserve_order(preserve_order)
 				{
 					if(n_prealloc > 0)
 						this->Prealloc(n_prealloc);
